efe9a4595abcd904a4ef2473432f86b637cb96ee
lixiangrong <lixiangrong@pxene.com>
2016-03-07 16:01:38 +0800

    新增文件

---
+++ src/main/java/osc/git/eh3/annotation/TestPkgAnnotation.java
@@ -0,0 +1,18 @@
+package osc.git.eh3.annotation;
+
+import java.lang.annotation.Annotation;
+
+public class TestPkgAnnotation {
+
+	public static void main(String[] args) {
+		Package pkg = Package.getPackage("osc.git.eh3.annotation");
+		Annotation[] annotations = pkg.getAnnotations();
+		for (Annotation annotation : annotations) {
+			System.out.println(annotation);
+		}
+
+		// ===========================友好类和包内访问常量==============
+		new MyPackageMethod().myPackageMethod();
+		System.out.println(MyPackageConst.PACKAGE_STRING);
+	}
+}
