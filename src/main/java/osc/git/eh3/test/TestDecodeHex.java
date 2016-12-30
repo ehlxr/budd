9429c1a8bda991c770e990c2c73769c9f3433f0d
ehlxr <ehlxr.me@gmail.com>
2016-12-30 22:35:58 +0800

    update Anaconda.sublime-settings

--- src/main/java/osc/git/eh3/test/TestDecodeHex.java
+++ src/main/java/osc/git/eh3/test/TestDecodeHex.java
@@ -8,7 +8,7 @@ import org.apache.commons.codec.binary.Hex;
 public class TestDecodeHex {
     // 十六进制转字符串
     public static void main(String[] args) throws Exception {
-        String data = "E7A7AFE69E81E58AA0E5BFABE7A791E68A80";
+        String data = "E88194E7B3BBE4BABAE6B7BBE58AA0E5A4B1E8B4A5";
         System.out.println(new String(Hex.decodeHex(data.toCharArray()), "utf-8"));
 
         System.out.println(Hex.encodeHexString("测试粗我".getBytes()));
