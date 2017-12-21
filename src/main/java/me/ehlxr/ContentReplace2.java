3fb067f7094c3e26c4dbfe76e679a6a4a5c23a79
ehlxr <ehlxr.me@gmail.com>
2017-12-21 17:18:54 +0800

    站点更新：2017-12-21 17:18:54

---
+++ src/main/java/me/ehlxr/ContentReplace2.java
@@ -0,0 +1,48 @@
+package me.ehlxr;
+
+import java.io.*;
+
+@SuppressWarnings("ALL")
+public class ContentReplace2 {
+    public static void main(String[] args) {
+        File file = new File("/Users/ehlxr/ehlxr/blog/hugoBlog/content/post");
+        File[] files = file.listFiles();
+
+        for (File f : files) {
+            operationFile(f);
+        }
+    }
+
+    private static void operationFile(File file) {
+        File tmpfile = new File(file.getParentFile().getAbsolutePath() + "\\" + file.getName() + ".tmp");
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(tmpfile))) {
+
+            boolean flag = false;
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                if (line.startsWith("title")) {
+                    System.out.println(line);
+                    // StringBuilder sb = new StringBuilder();
+                    // line = sb.append("title: \"").append(line.substring(line.indexOf("title") + 7)).append("\"").toString();
+                    line = line.replaceAll("'", "");
+                    System.out.println(line);
+
+                    flag = true;
+                }
+                writer.write(line + "\n");
+            }
+
+            if (flag) {
+                file.delete();
+                tmpfile.renameTo(new File(file.getAbsolutePath()));
+            } else {
+                tmpfile.delete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
