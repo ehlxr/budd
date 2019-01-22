4c53ecbbe529be8f2255dd5b8166d475097569fd
ehlxr <ehlxr.me@gmail.com>
2019-01-22 17:31:07 +0800

    add rabbitMQ demo

---
+++ src/main/java/me/ehlxr/rabbitmq/routing/Sender.java
@@ -0,0 +1,33 @@
+package me.ehlxr.rabbitmq.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import me.ehlxr.rabbitmq.ConnectionUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 发送消息到交换机并且要指定路由 key ，消费者将队列绑定到交换机时需要指定路由 key
+ *
+ * @author lixiangrong
+ * @since 2019-01-22.
+ */
+public class Sender {
+    private final static String EXCHANGE_NAME = "exchange_direct";
+    private final static String EXCHANGE_TYPE = "direct";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, EXCHANGE_TYPE);
+
+        String message = "那一定是蓝色";
+        channel.basicPublish(EXCHANGE_NAME, "key2", null, message.getBytes());
+        System.out.println("[x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/me/ehlxr/rabbitmq/simple/Receiver.java
@@ -0,0 +1,38 @@
+package me.ehlxr.rabbitmq.simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.QueueingConsumer;
+import me.ehlxr.rabbitmq.ConnectionUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 简单模式：一个生产者，一个消费者
+ *
+ * @author lixiangrong
+ * @since 2019-01-22.
+ */
+public class Receiver {
+    private final static String QUEUE_NAME = "simple_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        //获取连接
+        Connection connection = ConnectionUtil.getConnection();
+        //获取通道
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        while (true) {
+            //该方法会阻塞
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            System.out.println("[x] Received '" + message + "'");
+
+        }
+    }
+}
