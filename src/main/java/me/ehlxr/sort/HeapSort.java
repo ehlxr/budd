f46eb5c36b850a97448651477489444e002b7362
ehlxr <ehlxr.me@gmail.com>
2020-10-17 22:38:29 +0800

    add heap sort

--- src/main/java/me/ehlxr/sort/HeapSort.java
+++ src/main/java/me/ehlxr/sort/HeapSort.java
@@ -30,10 +30,46 @@ public class HeapSort {
     public static void main(String[] args) {
         int[] arr = {4, 9, 1, 8, 6, 2};
         sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void sort(int[] arr) {
+        // 1. 构建大顶堆
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            // 从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+        // 2. 调整堆结构+交换堆顶元素与末尾元素
+        for (int j = arr.length - 1; j > 0; j--) {
+            // 将堆顶元素与末尾元素进行交换
+            swap(arr, 0, j);
+            // 重新对堆进行调整
+            adjustHeap(arr, 0, j);
+        }
+    }
 
+    /**
+     * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     */
+    public static void adjustHeap(int[] arr, int i, int length) {
+        // 先取出当前元素 i
+        int temp = arr[i];
+        // 从 i 结点的左子结点开始，也就是 2i+1 处开始
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
+            // 如果左子结点小于右子结点，k 指向右子结点
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
+                k++;
+            }
+            // 如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+            if (arr[k] > temp) {
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        // 将 temp 值放到最终的位置
+        arr[i] = temp;
     }
 
     public static void swap(int[] arr, int i, int j) {
