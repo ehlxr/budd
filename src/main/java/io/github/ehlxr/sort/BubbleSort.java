5865722a7d8ff93c65710e1461330445d5ff7330
ehlxr <ehlxr.me@gmail.com>
2021-06-29 18:20:12 +0800

    add sort coast time test

--- src/main/java/io/github/ehlxr/sort/BubbleSort.java
+++ src/main/java/io/github/ehlxr/sort/BubbleSort.java
@@ -24,9 +24,6 @@
 
 package io.github.ehlxr.sort;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * 冒泡排序
  * <p>
@@ -78,21 +75,13 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        // int[] arr = {4, 9, 1, 8, 6, 2};
         int[] arr = new int[80000];
         for (int i = 0; i < 80000; i++) {
             arr[i] = (int) (Math.random() * 8000000); //生成一个[0, 8000000) 数
         }
 
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
+        long startTime = System.currentTimeMillis();
         sort(arr);
-
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序后的时间是=" + date2Str);
+        System.out.printf("排序花费时间 %dms.", System.currentTimeMillis() - startTime);
     }
 }
--- src/main/java/io/github/ehlxr/sort/InsertSort.java
+++ src/main/java/io/github/ehlxr/sort/InsertSort.java
@@ -24,9 +24,7 @@
 
 package io.github.ehlxr.sort;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 
 /**
  * 插入排序
@@ -90,21 +88,13 @@ public class InsertSort {
     }
 
     public static void main(String[] args) {
-        // int[] arr = {4, 9, 1, 8, 6, 2};
         int[] arr = new int[80000];
         for (int i = 0; i < 80000; i++) {
             arr[i] = (int) (Math.random() * 8000000); //生成一个[0, 8000000) 数
         }
 
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
+        long startTime = System.currentTimeMillis();
         sort(arr);
-
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序后的时间是=" + date2Str);
+        System.out.printf("排序花费时间 %dms.", System.currentTimeMillis() - startTime);
     }
 }
